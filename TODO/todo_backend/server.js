83a8f801af1b0d1e97536df4e2e66aff22325d54
Ali Tahir <alitahir243715@gmail.com>
2026-05-07 15:20:26 +0500

    Refactor server.js with improved MongoDB connection
    Refactor server.js to improve structure and error handling.

--- TODO/todo_backend/server.js
+++ TODO/todo_backend/server.js
@@ -4,51 +4,99 @@ const cors = require('cors');
 const TodoModel = require('./models/Todo');
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/TODO',
-    console.log('MongoDB connected')
-)
+/*
+|--------------------------------------------------------------------------
+| MongoDB Connection
+|--------------------------------------------------------------------------
+*/
 
-app.listen(5000,
-    console.log('Server listening on port: 5000')
-)
+mongoose.connect("mongodb://mongodb:27017/TODO")
+.then(() => {
+    console.log("MongoDB connected");
+})
+.catch((err) => {
+    console.log("MongoDB connection error:", err);
+});
 
+/*
+|--------------------------------------------------------------------------
+| Server
+|--------------------------------------------------------------------------
+*/
+
+app.listen(5000, () => {
+    console.log("Server listening on port: 5000");
+});
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+*/
+
+// Add Todo
 app.post('/add', (req, res) => {
-  const { task } = req.body;
-  TodoModel.create({ task })
-      .then(result => res.json(result))
-      .catch(err => console.log(err));
-   
+
+    const { task } = req.body;
+
+    TodoModel.create({ task })
+        .then(result => res.json(result))
+        .catch(err => res.json(err));
 });
 
-app.get('/get',(req,res)=>{
-  TodoModel.find()
-  .then(result=> res.json(result))
-  .catch(err=>console.log(err));
+
+// Get Todos
+app.get('/get', (req, res) => {
+
+    TodoModel.find()
+        .then(result => res.json(result))
+        .catch(err => res.json(err));
 });
-  
-app.put('/edit/:id',(req,res)=>{
-  const{id} = req.params;
-  TodoModel.findByIdAndUpdate(id,{done:true},{new:true})
-  .then(result=> res.json(result))
-  .catch(err=>res.json(err));
- });
 
-app.put('/update/:id',(req,res)=>{
-  const{id} = req.params;
-  const{task} = req.body;
-  TodoModel.findByIdAndUpdate(id,{task:task})
-  .then(result=> res.json(result))
-  .catch(err=>res.json(err));
- });
 
-app.delete('/delete/:id',(req,res)=>{
-  const{id} = req.params;
-  TodoModel.findByIdAndDelete({_id:id})
-  .then(result=> res.json(result))
-  .catch(err=>res.json(err));
- }); 
+// Mark Todo Done
+app.put('/edit/:id', (req, res) => {
 
-module.exports=app;
+    const { id } = req.params;
+
+    TodoModel.findByIdAndUpdate(
+        id,
+        { done: true },
+        { new: true }
+    )
+    .then(result => res.json(result))
+    .catch(err => res.json(err));
+});
+
+
+// Update Todo Text
+app.put('/update/:id', (req, res) => {
+
+    const { id } = req.params;
+    const { task } = req.body;
+
+    TodoModel.findByIdAndUpdate(
+        id,
+        { task: task },
+        { new: true }
+    )
+    .then(result => res.json(result))
+    .catch(err => res.json(err));
+});
+
+
+// Delete Todo
+app.delete('/delete/:id', (req, res) => {
+
+    const { id } = req.params;
+
+    TodoModel.findByIdAndDelete({ _id: id })
+        .then(result => res.json(result))
+        .catch(err => res.json(err));
+});
+
+module.exports = app;
